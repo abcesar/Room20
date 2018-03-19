94dc607a70805b3a1f03e6044d7a004fb4d1c8c4
Cesar Gutierrez <cesar@Cesars-MacBook-Pro.local>
2018-03-19 16:49:49 -0700

    Player Options

--- Room20/Room20.c
+++ Room20/Room20.c
@@ -7,5 +7,16 @@
 
 int main(void)
 {
+    int option;
     printf("You enter the door labled Room 20");
+    
+    printf("You walk through the door and enter a long narrow hallway\n")
+    printf("Press: \n"
+           "1: Just Stand there
+           "2: Walk Halfway down the hall and look at the clock to your left"
+           "3: Walk to the end of the hall and look at the table to your left"
+           "4: Walk to the end of the hall and turn right towards the rest of the hall");
+    scanf("%d",&option);
+    
+    
 }
