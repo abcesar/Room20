a37b30cd30b7fc0b8f218052658c2f1c0128f9e2
Cesar Gutierrez <cesar@Cesars-MacBook-Pro.local>
2018-03-19 17:00:39 -0700

    Seperate call

--- Room20/Room20.c
+++ Room20/Room20.c
@@ -7,12 +7,9 @@ void option1(option);
 
 int main(void)
 {
-    int option;
     printf("You enter the door labled Room 20");
     
     printf("You walk through the door and enter a long narrow hallway\n");
-    
-    option1(option);
     
 }
 
@@ -24,4 +21,21 @@ void option1(option)    //Function to Swaps Array Values
            "3: Walk to the end of the hall and look at the table to your left\n"
            "4: Walk to the end of the hall and turn right towards the rest of the hall\n");
     scanf("%d",&option);
+    
+    switch(options)
+    {
+        case 1:
+            option1(option);
+            break;
+            
+        case 2:
+            printf("You walk half way down the hall and look right")
+            break;
+        case 3:
+            printf("You walk to the end of the hall and look left");
+            break;
+        case 4:
+            printf("You walk to the end of the hall and turn right");
+            break
+    }
 }
