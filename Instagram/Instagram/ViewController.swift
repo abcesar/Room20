2344c7ee1d89c751a0cc389cc3574ce243eb1852
Cesar Gutierrez <cgutierrez152@toromail.csudh.edu>
2018-10-05 18:49:48 -0700

    Added icon

---
+++ Instagram/Instagram/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Cesar Gutierrez on 10/1/18.
+//  Copyright © 2018 Cesar Gutierrez. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    
+}
+
